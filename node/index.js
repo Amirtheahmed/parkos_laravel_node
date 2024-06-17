fd0a109ffd723d524fae0e9b1a5f366b0689d58b
amirahmed <amirtheahmed@gmail.com>
2024-06-17 15:38:52 +0300

    fixed bugs with tests, implemeted small node service to consume messages from rabbitmq

--- node/index.js
+++ node/index.js
@@ -1,1 +1,38 @@
-console.log("Put your email microservice here...");
+const amqp = require('amqplib');
+
+async function startConsumer() {
+    const queueName = 'reservation_email_queue';
+    const connectionString = 'amqp://localhost';
+
+    try {
+        const connection = await amqp.connect(connectionString);
+        console.log('Connected to RabbitMQ');
+
+        const channel = await connection.createChannel();
+        console.log('Channel created');
+
+        await channel.assertQueue(queueName, {
+            durable: true
+        });
+        console.log(`Queue ${queueName} is ready`);
+
+        console.log(`Waiting for messages in ${queueName}. To exit press CTRL+C`);
+        await channel.consume(queueName, message => {
+            if (message !== null) {
+                const content = message.content.toString();
+                const decoded = JSON.parse(content);
+                console.log("Received:", decoded);
+
+                // trigger email dispatch from here
+
+                // ack message
+                channel.ack(message);
+            }
+        });
+
+    } catch (error) {
+        console.error('Failed to connect or process messages:', error);
+    }
+}
+
+startConsumer();
